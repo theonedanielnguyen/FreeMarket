036eff7c33cc7690ee53b9f6cca0fdb7054004ca
Daniel Nguyen <theonedanielnguyen@gmail.com>
2021-03-11 22:08:43 -0800

    Built out more of the Store Information Page
    Work on submiut tomorrow

--- client/src/components/StoreInformationForm.js
+++ client/src/components/StoreInformationForm.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Form, Header } from 'semantic-ui-react';
+import { Button, Form, Header, Segment } from 'semantic-ui-react';
 
 const StoreInformationForm = () => {
     const shop = useSelector(state => state.userShop);
 
     const [ description, setDescription ] = useState(shop.description);
+    const [ editable, setEditable ] = useState(false);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+    }
+
+    const handleCancel = () => {
+        setDescription(shop.description);
+        setEditable(false);
     }
 
     return (
@@ -17,7 +23,50 @@ const StoreInformationForm = () => {
                 Store Information
             </Header>
             <Form onSubmit={handleSubmit}>
-
+                <Segment raised>
+                    {editable ? 
+                        <>
+                        <Form.Input fluid
+                            name='description'
+                            type='text'
+                            placeholder='Store Description'
+                            value={description}
+                            onChange={(e)=>setDescription(e.target.value)}
+                        />
+                        <Button.Group fluid>
+                            <Button
+                                type='button'
+                                size='large'
+                                content='Cancel'
+                                color='red'
+                                onClick={()=>{handleCancel()}}
+                            />
+                            <Button.Or />
+                            <Button
+                                type='submit'
+                                size='large'
+                                content='Save Description'
+                                color='green'
+                            />
+                        </Button.Group>
+                        </>
+                        :
+                        <>
+                        <Form.Input fluid readOnly
+                            name='description'
+                            placeholder='Store Description'
+                            value={description}
+                            type='text'
+                        />
+                        <Button fluid
+                            size='large'
+                            content='Edit Description'
+                            color='green'
+                            onClick={()=>setEditable(true)}
+                        />
+                        </>
+                    }
+                </Segment>
             </Form>
         </>
     )
