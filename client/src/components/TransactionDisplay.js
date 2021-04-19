7b84fe272362844381b2d7ac0fdd6384e75938b7
Daniel Nguyen <theonedanielnguyen@gmail.com>
2021-04-18 23:50:14 -0700

    Troubleshooting Transaction Display
    Still has issues with the way it is displaying

--- client/src/components/TransactionDisplay.js
+++ client/src/components/TransactionDisplay.js
@@ -1,34 +1,38 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Segment } from 'semantic-ui-react';
+import { Container, Segment } from 'semantic-ui-react';
 import ProductCardHorizontal from './ProductCardHorizontal';
 
 const TransactionDisplay = (props) => {
     const transactionID = props.transactionID;
     const [ transaction, setTransaction ] = useState({});
+    const [ loaded, setLoaded ] = useState(false);
 
     useEffect(() => {
         const loadTransaction = async () => {
             const APICall = await axios.get("http://localhost:8000/api/transaction/"+transactionID);
             setTransaction(APICall.data[0]);
             console.log(APICall.data[0]);
+            setLoaded(true);
         }
         loadTransaction();
     }, [transactionID])
 
     return (
-        <Segment>
-            {Object.keys(transaction.items).map((item, key) => {
-                return(
-                    transaction.items[item].map((subItem, subKey) => {
-                        return(
-                            <ProductCardHorizontal key={subKey} productID={subItem} deleteable={false}/>
-                        )
-                    })
-                )
-            })
-            }
-        </Segment>
+        <Container>
+        {loaded && Object.keys(transaction.items).map((item, key) => {
+            return(
+                <Segment>
+                {transaction.items[item].map((subItem, subKey) => {
+                    return(
+                        <ProductCardHorizontal key={subKey} productID={subItem} deleteable={false}/>
+                    )
+                })}
+                </Segment>
+            )
+        })
+        }
+        </Container>
     )
 }
 
