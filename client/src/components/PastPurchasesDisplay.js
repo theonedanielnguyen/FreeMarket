11b27b55b2de1ba4b2c8a8dae1c01c272d0c35cc
Daniel Nguyen <theonedanielnguyen@gmail.com>
2021-04-26 23:59:29 -0700

    Working on past purchases

--- client/src/components/PastPurchasesDisplay.js
+++ client/src/components/PastPurchasesDisplay.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { Container } from 'semantic-ui-react';
+import TransactionDisplay from './TransactionDisplay';
 
 const PastPurchasesDisplay = () => {
+    const user = useSelector(state => state.loggedInUser);
+
     return(
         <Container fluid>
-
+            {user.transactions_buyer.map((transaction, key) => {
+                return(
+                    <TransactionDisplay transactionID={transaction} key={key} />
+                )
+            })}
         </Container>
     )
 }
