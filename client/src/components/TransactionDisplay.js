06ea830cbcd079a1527dd81524c5fd0df6baf817
Daniel Nguyen <theonedanielnguyen@gmail.com>
2021-05-04 23:52:48 -0700

    Transaction Display amended, testing needed

--- client/src/components/TransactionDisplay.js
+++ client/src/components/TransactionDisplay.js
@@ -18,87 +18,85 @@ const TransactionDisplay = (props) => {
         const loadTransaction = async () => {
             const APICall = await axios.get("http://localhost:8000/api/transaction/"+transactionID);
             const tempTransaction = APICall.data[0];
-            setTransaction(tempTransaction);
             // console.log(APICall.data[0]);
             if (!purchases) {
                 // Parse item data for items
                 for (const userID in tempTransaction.items) {
-
+                    if (userID === user._id) {
+                        setUserItems(tempTransaction.items[userID])
+                    }
                 }
                 // Return only items of owner
             }
+            setTransaction(tempTransaction);
             setLoaded(true);
         }
         loadTransaction();
-    }, [transactionID])
+    }, [transactionID, purchases, user])
 
     return (
         <Container centered>
         <Segment compact padded='very' raised>
-        {loaded && Object.keys(transaction.items).map((item, key) => {
+        {loaded && purchases? Object.keys(transaction.items).map((item, key) => {
             return(
-                <Container>
+                <Container key={key}>
                     <Grid centered>
-                        {purchases?
-                            <>
-                            {/* This is for PURCHASES */}
-                            <Grid.Row>
-                                <Item>
+                        <Grid.Row>
+                            <Item>
+                                <Item.Content>
+                                    <Item.Description style={{textAlign:'right'}}>
+                                        <h3><strong>Ordered On:</strong> {moment(transaction.updatedAt).format('MM/ DD/yyyy')}</h3>
+                                    </Item.Description>
+                                </Item.Content>
+                            </Item>
+                        </Grid.Row>
+                        <Grid.Row>
+                            <Item.Group divided style={{width:'60vw'}}>
+                                {transaction.items[item].map((subItem, subKey) => {
+                                    return(
+                                        <ProductCardHorizontal key={subKey} productID={subItem} deleteable={false}/>
+                                    )
+                                })}
+                            </Item.Group>
+                        </Grid.Row>
+                        <Grid.Row style={{textAlign:'right'}}>
+                                <Grid.Column>
+                                    <Item style={{marginRight: '20vw'}}>
                                     <Item.Content>
                                         <Item.Description style={{textAlign:'right'}}>
-                                            <h3><strong>Ordered On:</strong> {moment(transaction.updatedAt).format('MM/ DD/yyyy')}</h3>
+                                            <h3><strong>Total:</strong> $ {transaction.total}</h3>
                                         </Item.Description>
                                     </Item.Content>
-                                </Item>
-                            </Grid.Row>
-                            <Grid.Row>
-                                <Item.Group divided style={{width:'60vw'}}>
-                                    {transaction.items[item].map((subItem, subKey) => {
-                                        return(
-                                            <ProductCardHorizontal key={subKey} productID={subItem} deleteable={false}/>
-                                        )
-                                    })}
-                                </Item.Group>
-                            </Grid.Row>
-                            <Grid.Row style={{textAlign:'right'}}>
-                                    <Grid.Column>
-                                        <Item style={{marginRight: '20vw'}}>
-                                        <Item.Content>
-                                            <Item.Description style={{textAlign:'right'}}>
-                                                <h3><strong>Total:</strong> $ {transaction.total}</h3>
-                                            </Item.Description>
-                                        </Item.Content>
-                                        </Item>
-                                    </Grid.Column>
-                            </Grid.Row>
-                            </>
-                            :
-                            <>
-                            {/* This is for SALES */}
-                            <Grid.Row>
-                                <Item>
-                                    <Item.Content>
-                                        <Item.Description style={{textAlign:'right'}}>
-                                            <h3><strong>Sold On:</strong> {moment(transaction.updatedAt).format('MM/ DD/yyyy')}</h3>
-                                        </Item.Description>
-                                    </Item.Content>
-                                </Item>
-                            </Grid.Row>
-                            <Grid.Row>
-                                <Item.Group divided style={{width:'60vw'}}>
-                                    {transaction.items[item].map((subItem, subKey) => {
-                                        return(
-                                            <ProductCardHorizontal key={subKey} productID={subItem} deleteable={false}/>
-                                        )
-                                    })}
-                                </Item.Group>
-                            </Grid.Row>
-                            </>
-                        }
+                                    </Item>
+                                </Grid.Column>
+                        </Grid.Row>
                     </Grid>
                 </Container>
-            )
-        })
+                )
+            })
+            :
+            <Container>
+                <Grid centered>
+                    <Grid.Row>
+                        <Item>
+                            <Item.Content>
+                                <Item.Description style={{textAlign:'right'}}>
+                                    <h3><strong>Sold On:</strong> {moment(transaction.updatedAt).format('MM/ DD/yyyy')}</h3>
+                                </Item.Description>
+                            </Item.Content>
+                        </Item>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Item.Group divided style={{width:'60vw'}}>
+                            {userItems.map((item, key) => {
+                                return(
+                                    <ProductCardHorizontal key={key} productID={item} deleteable={false}/>
+                                )
+                            })}
+                        </Item.Group>
+                    </Grid.Row>
+                </Grid>
+            </Container>
         }
         </Segment>
         </Container>
